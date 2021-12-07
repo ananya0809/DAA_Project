377f3ec512cf19d4b13f75c3c0893b703b45e1ea
oddlyspaced <oddlyspaced1@gmail.com>
2021-12-07 22:13:13 +0530

    Election: Add sample counter hello world code

--- Election/src/main/kotlin/Main.kt
+++ Election/src/main/kotlin/Main.kt
@@ -1,13 +1,21 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
@@ -15,12 +23,23 @@ import androidx.compose.ui.window.application
 @Preview
 fun App() {
     var text by remember { mutableStateOf("Hello, World!") }
-
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        val count = remember { mutableStateOf(0) }
+        MaterialTheme {
+            Column(Modifier.fillMaxSize(), Arrangement.spacedBy(5.dp)) {
+                Button(modifier = Modifier.align(Alignment.CenterHorizontally),
+                    onClick = {
+                        count.value++
+                    }) {
+                    Text(if (count.value == 0) "Hello World" else "Clicked ${count.value}!")
+                }
+                Button(modifier = Modifier.align(Alignment.CenterHorizontally),
+                    onClick = {
+                        count.value = 0
+                    }) {
+                    Text("Reset")
+                }
+            }
         }
     }
 }
