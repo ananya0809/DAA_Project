3646bf60948809d86d833ccedf861317f6a9a64b
oddlyspaced <oddlyspaced1@gmail.com>
2021-12-08 02:09:31 +0530

    Election: Main: Setup base sample student list

--- Election/src/main/kotlin/Main.kt
+++ Election/src/main/kotlin/Main.kt
@@ -1,9 +1,12 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
@@ -36,10 +39,30 @@ fun start() {
 @Composable
 @Preview
 fun studentList() {
+    val sampleStudents = remember {
+        arrayListOf<Student>()
+    }
+    sampleStudents.add(Student("Name 1", "1222"))
+    sampleStudents.add(Student("Name 2", "1222"))
+    sampleStudents.add(Student("Name 3", "1222"))
     MaterialTheme {
-        Row (horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxSize().padding(24.dp)) {
-            Text(text = "Name", style = MaterialTheme.typography.h5)
-            Text(text = "Registration No.", style = MaterialTheme.typography.h5)
+        Column {
+            LazyColumn {
+                item {
+                    // TODO : Fix spacing between titles and items
+                    Row (horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxSize().padding(24.dp)) {
+                        Text(text = "Name", style = MaterialTheme.typography.h5)
+                        Text(text = "Registration No.", style = MaterialTheme.typography.h5)
+                    }
+                }
+                items(sampleStudents) { std ->
+                    Row {
+                        Text(text = std.name, modifier = Modifier.padding(horizontal = 24.dp))
+                        Text(text = std.regNum, modifier = Modifier.padding(horizontal = 24.dp))
+                    }
+                }
+            }
+
         }
     }
 }
