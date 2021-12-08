d8c079c23cd6626053ed2de450b12d118145e66f
oddlyspaced <oddlyspaced1@gmail.com>
2021-12-08 12:44:21 +0530

    Election: CRUtilsKT: Setup kotlin utils class

---
+++ Election/src/main/kotlin/CRUtilsKT.kt
@@ -0,0 +1,22 @@
+import androidx.compose.runtime.mutableStateOf
+
+object CRUtilsKT {
+    private const val GPA_CRITERIA = 8.5
+
+    fun findQualifyingStudents(students: ArrayList<Student>): ArrayList<QualifyingStudent> {
+        val qualifyingStudent = arrayListOf<QualifyingStudent>()
+        students.forEach {
+            if (it.GPA >= GPA_CRITERIA) {
+                qualifyingStudent.add(QualifyingStudent(it, mutableStateOf(0)))
+            }
+        }
+        return qualifyingStudent
+    }
+
+    fun findCR(students: ArrayList<QualifyingStudent>): QualifyingStudent? {
+        val max = students.maxByOrNull {
+            it.votes.value
+        }
+        return max
+    }
+}
