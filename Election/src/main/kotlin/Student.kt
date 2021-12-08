0eef597e55bd08d0866cbfe6caf0e10c366ca821
oddlyspaced <oddlyspaced1@gmail.com>
2021-12-08 11:25:46 +0530

    Election: Student: Add major detail parameters

--- Election/src/main/kotlin/Student.kt
+++ Election/src/main/kotlin/Student.kt
@@ -1,4 +1,10 @@
 data class Student(
     val name: String,
     val regNum: String,
+    val branch: String,
+    val course: String,
+    val year: String,
+    val section: String,
+    val GPA: Double,
+    val isDayScholar: Boolean
 )
