4f0c88c134cb378f9874d6dcd79589447143ff47
oddlyspaced <oddlyspaced1@gmail.com>
2021-12-08 11:15:55 +0530

    Election: Main: Setup dynamic Ask Vote screen

--- Election/src/main/kotlin/Main.kt
+++ Election/src/main/kotlin/Main.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -47,9 +49,6 @@ val sampleStudents = mutableStateListOf<Student>()
 @Composable
 @Preview
 fun TotalStudentList() {
-    sampleStudents.add(Student("Name 1", "1222"))
-    sampleStudents.add(Student("Name 2", "1222"))
-    sampleStudents.add(Student("Name 3", "1222"))
     MaterialTheme {
         // TODO : Figure out a way to make Add button position constant and independent
         Column {
@@ -91,13 +90,13 @@ fun TotalStudentList() {
 }
 
 // Qualifying Students [Sublist of Total Students]
-val qualifyingStudents = mutableStateListOf<QualifyingStudent>()
+val qualifyingStudents = ArrayList<QualifyingStudent>()
 @Composable
 @Preview
 fun QualifyingStudentList() {
     sampleStudents.forEach { student ->
         // Perform this in a separate function and apply qualifying logic
-        qualifyingStudents.add(QualifyingStudent(student, 0))
+        qualifyingStudents.add(QualifyingStudent(student, mutableStateOf(0)))
     }
     MaterialTheme {
         // TODO : Figure out a way to make Add button position constant and independent
@@ -134,8 +133,65 @@ fun QualifyingStudentList() {
     }
 }
 
+@Composable
+@Preview
+fun AskVotes() {
+    sampleStudents.add(Student("Name 1", "1222"))
+    sampleStudents.add(Student("Name 2", "1222"))
+    sampleStudents.add(Student("Name 3", "1222"))
+
+    sampleStudents.forEach { student ->
+        // Perform this in a separate function and apply qualifying logic
+        qualifyingStudents.add(QualifyingStudent(student, mutableStateOf(0)))
+    }
+
+    MaterialTheme {
+        // TODO : Figure out a way to make Add button position constant and independent
+        Column {
+            LazyColumn {
+                item {
+                    // TODO : Fix spacing between titles and items
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier.fillMaxSize().padding(24.dp)
+                    ) {
+                        Text(text = "Name", style = MaterialTheme.typography.h5)
+                        Text(text = "Registration No.", style = MaterialTheme.typography.h5)
+                    }
+                }
+                items(items = qualifyingStudents) { std ->
+                    Row {
+                        Text(text = std.student.name, modifier = Modifier.padding(horizontal = 24.dp))
+                        Text(text = std.student.regNum, modifier = Modifier.padding(horizontal = 24.dp))
+                        TextField(
+                            // TODO : Replace index of with something more robus
+                            value = std.votes.value.toString(),
+                            onValueChange = {
+                                std.votes.value = it.toInt()
+                            },
+                            label = {
+                                Text("Number of Votes")
+                            }
+                        )
+                    }
+                }
+            }
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxSize().padding(24.dp),
+            ) {
+                Button(onClick = {
+                    // Go to next Screen
+                }) {
+                    Text("Next")
+                }
+            }
+        }
+    }
+}
+
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
-        QualifyingStudentList()
+        AskVotes()
     }
 }
