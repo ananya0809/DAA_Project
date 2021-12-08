b96058a03b27b87aa95d3b57e1426e0cae86eab1
oddlyspaced <oddlyspaced1@gmail.com>
2021-12-08 11:56:17 +0530

    Election: CRUtils: Setup sample findCR method with logic

--- Election/src/main/kotlin/CRUtils.java
+++ Election/src/main/kotlin/CRUtils.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+
 public class CRUtils {
+
+    private static final double GPA_CRITERIA = 8.5;
+
     // Finds a CR from a qualifying list of students
-    public static QualifyingStudent findCR() {
-        return null;
+    public static ArrayList<QualifyingStudent> findCR(ArrayList<QualifyingStudent> students) {
+        ArrayList<QualifyingStudent> selected = new ArrayList<>();
+        for (QualifyingStudent std : students) {
+            if (std.component1().getGPA() >= GPA_CRITERIA) {
+                selected.add(std);
+            }
+        }
+        return selected;
     }
 }
